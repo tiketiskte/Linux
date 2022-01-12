ce1227c1e78b67c79f8cdbd3dd6dd4dc878886de
tiketiskte <15092524952@163.com>
2022-01-12 23:27:13 +0800

    csapp

--- msstore.c
+++ msstore.c
@@ -1,21 +1,6 @@
 #include <stdio.h>
 
-long mult2(long a, long b)
-{
-    long s = a * b;
-    return s;
-}
-
-void multstore(long x, long y, long *dest)
-{
-    long t = mult2(x, y);
-    *dest = t;
-}
-
-int main()
-{
-    long d;
-    multstore(2, 3, &d);
-    //printf("2 * 3 --> %ld\n", d);
+int main(void) {
+    printf("%d %d\n", 5/2, -5/2);
     return 0;
 }
