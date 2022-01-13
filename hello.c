2ccb00c3207ef596d964eafb0d623a904ad594ba
tiketiskte <15092524952@163.com>
2022-01-13 12:12:42 +0800

    practice 2.42

--- hello.c
+++ hello.c
@@ -1,12 +1,11 @@
+//practice 2.42
 #include <stdio.h>
+int div16(int x) {
+	int bias = (x >> 31) & 0xF;
+	return (x + bias) >> 4;
+}
 
-int main(void)
-{
-	int n = 100;
-	for (int i = 1; i < n; i++)
-	{
-		//qwq
-		printf("%d\n", i);
-	}
+int main(void) {
+	printf("%d\n", div16(-12340));
 	return 0;
 }
