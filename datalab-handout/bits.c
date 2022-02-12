a7ee235e84b8e18759153fa486e8b95b19fc9817
tiketiskte <15092524952@163.com>
2022-02-12 17:02:24 +0800

    isAsciDigit

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -229,7 +229,10 @@ int isAsciiDigit(int x) {
  *   Rating: 3
  */
 int conditional(int x, int y, int z) {
-  return 2;
+ // int mask = ((!!x) << 31) >>31;
+  int mask = ~(!!x) + 1;
+  return (~mask & z) | (mask & y);
+ // return 2;
 }
 /* 
  * isLessOrEqual - if x <= y  then return 1, else return 0 
