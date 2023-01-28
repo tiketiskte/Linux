b38bebb9c915ddb0c4b1eeffc8b0f14f7a93ef0a
tiketiskte <15092524952@163.com>
2023-01-28 09:55:18 +0800

    no_stack_protector

---
+++ Chapter3/gets.s
@@ -0,0 +1,96 @@
+	.file	"gets.c"
+	.text
+	.globl	gets
+	.type	gets, @function
+gets:
+.LFB23:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	pushq	%rbx
+	.cfi_def_cfa_offset 24
+	.cfi_offset 3, -24
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 32
+	movq	%rdi, %rbp
+	movq	%rdi, %rbx
+	jmp	.L2
+.L4:
+	movb	%al, (%rbx)
+	leaq	1(%rbx), %rbx
+.L2:
+	movq	stdin(%rip), %rdi
+	call	getc@PLT
+	cmpl	$10, %eax
+	je	.L3
+	cmpl	$-1, %eax
+	jne	.L4
+.L3:
+	cmpl	$-1, %eax
+	sete	%dl
+	cmpq	%rbp, %rbx
+	sete	%al
+	testb	%al, %dl
+	jne	.L6
+	movb	$0, (%rbx)
+	movq	%rbp, %rax
+.L1:
+	addq	$8, %rsp
+	.cfi_remember_state
+	.cfi_def_cfa_offset 24
+	popq	%rbx
+	.cfi_def_cfa_offset 16
+	popq	%rbp
+	.cfi_def_cfa_offset 8
+	ret
+.L6:
+	.cfi_restore_state
+	movl	$0, %eax
+	jmp	.L1
+	.cfi_endproc
+.LFE23:
+	.size	gets, .-gets
+	.globl	echo
+	.type	echo, @function
+echo:
+.LFB24:
+	.cfi_startproc
+	endbr64
+	pushq	%rbx
+	.cfi_def_cfa_offset 16
+	.cfi_offset 3, -16
+	subq	$16, %rsp
+	.cfi_def_cfa_offset 32
+	leaq	8(%rsp), %rbx
+	movq	%rbx, %rdi
+	call	gets
+	movq	%rbx, %rdi
+	call	puts@PLT
+	addq	$16, %rsp
+	.cfi_def_cfa_offset 16
+	popq	%rbx
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE24:
+	.size	echo, .-echo
+	.ident	"GCC: (Ubuntu 11.3.0-1ubuntu1~22.04) 11.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
